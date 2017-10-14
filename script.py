d12697ea3c3b3227e02ef0ed3087a684e15843ba
Jacob <jacobklitzke@gmail.com>
2017-10-14 15:14:13 -0400

    Fixed email script again

--- script.py
+++ script.py
@@ -1,13 +1,41 @@
-import os
-import shutil
+import requests
 import sys
+import smtplib
 
-path = '/mnt/files/Movies/'
-#path = '/home/jacob/test/'
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-for foldername, subfolders, filenames in os.walk(path):
-	print foldername
-	print filenames
-	for filename in filenames:
-		#shutil.move(foldername + '/' + filename, '../' + filename)
-		os.rename(foldername + '/' + filename, path + filename)
+EMAIL=''
+EMAIL_USERNAME = ''
+EMAIL_PASSWORD = ''
+
+me = EMAIL
+you = EMAIL
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Script Results"
+msg['From'] = me
+msg['To'] = you
+
+res = requests.get('http://localhost:5000/findItems')
+if res.content == "NoResults":
+    sys.exit()
+else:
+    results = res.json()
+    html = ("<html><head><style>table,th,td {border: 1px solid black;}</style></head><body><table>" +
+            "<tr><th>Title</th><th>Price</th><th>Listing Type</th><th>Time Left</th><th>Link</th></tr>")
+    for result in results:
+        html = (html + "<tr><th>" + result["title"] + "</th>" +
+                "<th>" + str(result["price"]) + "</th>" +
+                "<th>" + result["listingType"] + "</th>" +
+                "<th>" + result["timeLeft"] + "</th>" +
+                "<th><a href=\"" + result["link"] + "\">Link</a></th></tr>")
+    html = html + "</table></body></html>"
+    part = MIMEText(html, 'html', 'utf-8')
+    msg.attach(part)
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(EMAIL_USERNAME, EMAIL_PASSWORD)
+    mail.sendmail(me, you, msg.as_string())
+    mail.quit()
